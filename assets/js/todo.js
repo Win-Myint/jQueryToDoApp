a84ceb94e12a779b13ccc5ce3543bff44f47b5d8
Win <win.m179@gmail.com>
2017-04-05 16:44:23 +0100

    added a function that allows users to add a new todo and append to li

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,14 +1,27 @@
 // Strike-through individual li when clicked
-$('li').click(function() {
+$('ul').on('click', 'li', function() {
 	$(this).toggleClass("liCompleted")
 })
 
 // Delete selected li when clicked
 // Be aware of event bubbling
-$('span').click(function(event) {
+$('ul').on('click', 'span', function(event) {
 	$(this).parent().fadeOut(500, function() {
 		$(this).remove();
 	})
 	// Prevents the event from bubbling up the DOM tree
 	event.stopPropagation();
 }) 
+
+// Adding new todo 
+$("input[type = 'text']").keypress(function(event) {
+	// Check if key press is equal to ENTER (13)
+	if(event.which === 13) {
+		var newToDo = $(this).val();
+		// Clear input field
+		$('input').val('');
+
+		// Add newToDo at the end of li
+		$('ul').append('<li><span>X</span> ' + newToDo + '</li>')
+	}
+})
