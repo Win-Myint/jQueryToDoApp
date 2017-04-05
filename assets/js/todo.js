8f7cde65bdd7affd7ddf93074c10cd6457b201ae
Win <win.m179@gmail.com>
2017-04-05 15:09:55 +0100

    added delete function to todo.js, applied fadeOut effect when deleting and stop event bubbling

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -2,3 +2,13 @@
 $('li').click(function() {
 	$(this).toggleClass("liCompleted")
 })
+
+// Delete selected li when clicked
+// Be aware of event bubbling
+$('span').click(function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	})
+	// Prevents the event from bubbling up the DOM tree
+	event.stopPropagation();
+}) 
